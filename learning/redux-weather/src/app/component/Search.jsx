5ae9dfc15f52e5f16e32a6d24fcfa18f54f6ffbc
Jeffery <j306796405@163.com>
2016-08-12 16:59:36 +0800

    read # React Redux Starter Kit

--- learning/redux-weather/src/app/component/Search.jsx
+++ learning/redux-weather/src/app/component/Search.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import { VelocityComponent } from 'velocity-react'
 
 import 'velocity-animate/velocity.ui'
 
 export default class Search extends React.Component {
+
   render() {
     return (
-      <VelocityComponent enter={{animation: "slideDown"}} leave={{animation: "slideUp"}}>
-        <div className="page navbar-fixed">
-          <div className="navbar">
-            <div className="navbar-inner">
-              <form className="searchbar">
-                <div className="searchbar-input">
-                  <input type="search" placeholder="Search"/><a href="#" className="searchbar-clear"></a>
-                  <div className="preloader preloader-white"></div>
+      <div className="popup">
+        <div className="view">
+          <div className="pages">
+            <div className="page navbar-fixed">
+              <div className="navbar">
+                <div className="navbar-inner">
+                  <form className="searchbar">
+                    <div className="searchbar-input">
+                      <input type="search" placeholder="Search" /><a href="#" className="searchbar-clear"></a>
+                        <div className="preloader preloader-white"></div>
+                    </div>
+                  </form>
                 </div>
-              </form>
-            </div>
-          </div>
-          <div className="page-content"> 
-            <div className="list-block">
-              <ul className="search-results"></ul>
+              </div>
+              <div className="page-content">
+                <div className="list-block">
+                  <ul className="search-results"></ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </VelocityComponent>
+      </div>
     )
   }
 }
